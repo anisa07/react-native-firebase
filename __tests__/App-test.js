fcb0242c3af2282582c3292cbcc1d3e36d60023f
anisa07 <askany07@gmail.com>
2020-04-09 13:35:40 +0300

    fix test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,20 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('@react-native-firebase/auth', () => {
+  return () => ({
+    onAuthStateChanged: jest.fn()
+  })
+});
+
+jest.mock('@react-native-firebase/firestore', () => ({}));
+jest.mock('@react-native-firebase/storage', () => ({}));
+
+import App from '../App';
+
 it('renders correctly', () => {
-  renderer.create(<App />);
+    renderer.create(<App/>);
 });
