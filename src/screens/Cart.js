c86f8b2c940658c72bb1aace7b0cc368aa8f2a70
anisa07 <askany07@gmail.com>
2020-04-09 11:44:48 +0300

    cart update

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -9,7 +9,6 @@ import {
 } from 'react-native';
 
 function CartScreen(props) {
-    console.log('props', props);
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>Cart!</Text>
